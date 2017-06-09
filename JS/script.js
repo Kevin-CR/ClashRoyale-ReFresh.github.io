a336c0a4886608acdb80147992c25964208966be
BlueFlame <kevscraze@gmail.com>
2017-06-09 21:32:51 +0200

    A LOT of stuff - animations and jQuery - added

--- JS/script.js
+++ JS/script.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function(){
 
+	$('#RF-logo').addClass('spin')
+
 	$('li a').click(function(){
 
 		$('li a').removeClass("active");
@@ -26,5 +28,77 @@ $(document).ready(function(){
 		$(".clanName").toggleClass("clan-name-display");
 
 	});
-});
+
+})
+
+$(document).ready(function(){
+
+	$('#21').click(function(){
+  	    $('#cir').animate({left: '631px'}, 300)
+    })
+
+    $('#31').click(function(){
+  	    $('#cir').animate({left: '827.6px'}, 300)
+    })
+
+    $('#12').click(function(){
+  	    $('#cir').animate({left: '435px'}, 300)
+    })
+
+    function select1() {
+	    $('.main-text').hide();
+	    $('#11').slideDown(800);
+    }
+
+    function select2() {
+	    $('.main-text').hide();
+	    $('#22').slideDown(800);
+    }
+
+	function select3() {
+		$('.main-text').hide();
+		$('#33').slideDown(800);
+	};
+
+})
+
+	$('#21').click(function(){
+  	    $('#cir').animate({left: '631px'}, 300)
+    })
+
+    $('#31').click(function(){
+  	    $('#cir').animate({left: '827.6px'}, 300)
+    })
+
+    $('#12').click(function(){
+  	    $('#cir').animate({left: '435px'}, 300)
+    })
+
+    function select1() {
+	    $('.main-text').hide();
+	    $('#11').slideDown(800);
+    }
+
+    function select2() {
+	    $('.main-text').hide();
+	    $('#22').slideDown(800);
+    }
+
+	function select3() {
+		$('.main-text').hide();
+		$('#33').slideDown(800);
+	};
+
+    $(function () {
+
+  		setTimeout(function() {
+  			$('.flew').removeClass('hide')
+  		}, 500);
+
+  	})
+
+
+
+
+
 
